16ef66e0a2b1415acc71de7adcdf1c2d9ff4ceea
VIJAY PRATAP <vijaypratap2020@gmail.com>
2025-07-06 17:02:05 +0000

    Register SqlService in DI container in Program.cs

--- QueryBotWeb/Program.cs
+++ QueryBotWeb/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<OpenAiService>();
+builder.Services.AddScoped<SqlService>();
 
 var app = builder.Build();
 

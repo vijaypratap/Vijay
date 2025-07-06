36565406e0b9c75c990f594f3c5998c04fb2b693
VIJAY PRATAP <vijaypratap2020@gmail.com>
2025-07-06 16:53:08 +0000

    Register OpenAiService in DI container in Program.cs

--- QueryBotWeb/Program.cs
+++ QueryBotWeb/Program.cs
@@ -1,7 +1,12 @@
+
+using QueryBotWeb.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<OpenAiService>();
 
 var app = builder.Build();
 
